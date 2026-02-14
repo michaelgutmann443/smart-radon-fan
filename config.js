982522426b024febc2c5cf30c5ccc5cfb884f38a
michaelgutmann443 <56284978+michaelgutmann443@users.noreply.github.com>
2026-02-14 14:41:37 -0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ window.SITE_CONFIG = {
   productName: "Smart Radon Fan",
   tagline: "Adaptive control for residential radon mitigation fans",
   contactName: "Michael J. Gutmann",
-  contactAddress: "1125 NE 3rd Avenue, Hillsboro, OR 97124",
   contactPhone: "503 869 9507",
-  contactEmail: "michaeljgutmann@gmail.com",
+  contactEmail: "info@smartradonfan.com",
 
