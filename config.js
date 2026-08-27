7dab1a018e7da4b17f4e052279397300c62d3a0c
michaelgutmann443 <56284978+michaelgutmann443@users.noreply.github.com>
2026-08-27 06:36:46 -0700

    Minor changes to VS site

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.SITE_CONFIG = {
   companyName: "Residential Radon Solutions, LLC",
   productName: "SmRF",
-  tagline: "Automated variable-rate radon mitigation",
+  tagline: "Automated variable-speed radon mitigation",
   contactName: "Michael J. Gutmann",
   contactAddress: "1125 NE 3rd Avenue, Hillsboro, OR 97124",
   contactPhone: "503 869 9507",
